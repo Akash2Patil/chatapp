7bf43d80d25511d36b8aebd4dc803acb529b7034
Akash2Patil <akashpatil1321999@gmail.com>
2025-10-04 08:34:49 +0530

    inp comp made

---
+++ chatapp-fe/src/components/Inputbox.tsx
@@ -0,0 +1,10 @@
+const Inputbox = () => {
+  return (
+    <div className="border border-blue-600 mx-auto rounded-xl flex">
+      <input className="border-none border-focus-none" type="text" placeholder="Enter Your Message" />
+      <button>Send</button>
+    </div>
+  )
+}
+
+export default Inputbox
