9e53db5df0b9992792047a1a871ad97a8dc6cbf4
Akash2Patil <akashpatil1321999@gmail.com>
2025-10-04 08:36:05 +0530

    chat ui updated

--- chatapp-fe/src/App.tsx
+++ chatapp-fe/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Chatarea from "./components/Chatarea"
+import Inputbox from "./components/Inputbox"
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className="bg-zinc-900 w-full h-screen text-white flex flex-col justify-center gap-5">
+      <Chatarea/>
+      <Inputbox/>
     </div>
   )
 }
